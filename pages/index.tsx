e17ab6ffd3b0ec41c7c0eec4d68e5f7fdfacfd06
Tikeswar Naik <Tikeswar.naik11@gmail.com>
2022-05-09 02:46:14 +0530

    project setup

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>FoodHub</title>
+        <meta name="description" content="Order delecious Food from our restaurant" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
